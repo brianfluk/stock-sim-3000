a93c547b63eb8345f1e54d92680c07a27037e6da
brianfluk <brianf.luk@gmail.com>
2020-07-05 00:01:32 -0400

    client proxy for heroku

--- server/server.js
+++ server/server.js
@@ -54,4 +54,6 @@ const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`Server Listening on ${port}`)
+  console.log(`NODE_ENV: "${process.env.NODE_ENV}"`)
+  console.log(`MONGO_URI: "${process.env.MONGO_URI}"`)
 });
